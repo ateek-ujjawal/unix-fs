f5c7b07d811f5a0c3e350f008d5ae51b7528c1d3
Haonan Lu <lu.haon@northeastern.edu>
2023-12-04 16:08:33 -0500

    remove unused code & fix

--- homework.c
+++ homework.c
@@ -294,33 +294,6 @@ void *lab3_init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
     data_blk = inode_region_blk + super->inodes_len;
     data_len = BLOCK_SIZE * super->blk_map_len;
 
-    /*
-    struct fs_inode inode;
-    for(int i = 0; i < inode_count; i++) {
-        inode = *(inode_tbl + i);
-        fprintf(stdout, "uid: %d\n", inode.uid);
-        fprintf(stdout, "gid: %d\n", inode.gid);
-        fprintf(stdout, "mode: %o\n", inode.mode);
-        fprintf(stdout, "mtime: %d\n", inode.mtime);
-        fprintf(stdout, "size: %d\n", inode.size);
-        for(int j = 0; j < n_direct; j++) {
-                fprintf(stdout, "ptrs[%d]: %d\n", j, inode.ptrs[j]);
-        }
-        fprintf(stdout, "indir_1: %d\n", inode.indir_1);
-        fprintf(stdout, "indir_2: %d\n", inode.indir_2);
-        fprintf(stdout, "\n");
-    }
-    */
-
-    /* for(int i = 0; i < super->blk_map_len * BLOCK_SIZE; i++) {
-        fprintf(stdout, "block bitmap bit %d is %d\n", i, bit_test(block_bmp,
-    i));
-    }
-    for(int i = 0; i < super->in_map_len * BLOCK_SIZE; i++) {
-        fprintf(stdout, "inode bitmap bit %d is %d\n", i, bit_test(inode_bmp,
-    i));
-    }*/
-
     return NULL;
 }
 
@@ -341,62 +314,6 @@ int lab3_getattr(const char *path, struct stat *sb, struct fuse_file_info *fi) {
     struct fs_inode *inode = inode_tbl + *inode_no;
     inode_2_stat(sb, inode);
     return 0;
-
-    /* Read tokens from path through parser */
-    /*
-    const int max_tokens = 32;
-    char *tokens[max_tokens], linebuf[1024];
-    int count = split_path(path, max_tokens, tokens, linebuf, sizeof(linebuf));
-
-
-    struct fs_inode inode;
-    int dir_in_blk = BLOCK_SIZE / sizeof(struct fs_dirent);
-    char *title = "";
-    */
-
-    /* Brute force search on all directory entries in the file system */
-    /*
-    for(int i = 0; i < count; i++) {
-        title = "";
-        int j = 0;
-loop:
-        while(strcmp(title, tokens[i]) && j < inode_count) {
-                inode = *(inode_tbl + j);
-                struct fs_dirent *dir_entries, dir_entry;
-                dir_entries = malloc(dir_in_blk * sizeof(struct fs_dirent));
-                memset(dir_entries, 0, dir_in_blk * sizeof(struct fs_dirent));
-                int type = (inode.mode & 0777000) >> 9;
-                if(bit_test(inode_bmp, j) && type == 0040) {
-                        for (int k = 0; k < N_DIRECT; k++) {
-                                if(inode.ptrs[k] >= inode_region_blk) {
-                                        block_read(dir_entries, inode.ptrs[k],
-1); for(int l = 0; l < dir_in_blk; l++) { dir_entry = *(dir_entries + l);
-                                                if(dir_entry.valid &&
-!strcmp(dir_entry.name, tokens[i])) { title = dir_entry.name; goto loop;
-                                                }
-                                        }
-                                }
-                        }
-                }
-                j++;
-        }
-    }
-
-    if(strcmp(title, "")) {
-        sb = malloc(sizeof(*sb));
-        memset(sb, 0, sizeof(*sb));
-        sb->st_mode = inode.mode;
-        sb->st_nlink = 1;
-        sb->st_uid = inode.uid;
-        sb->st_gid = inode.gid;
-        sb->st_size = inode.size;
-        sb->st_blocks = div_round_up(inode.size, BLOCK_SIZE);
-        sb->st_atime = sb->st_mtime = sb->st_ctime = inode.mtime;
-    } else {
-        return -ENOENT;
-    }
-    return 0;
-    */
 }
 
 typedef int (*fuse_fill_dir_t) (void *ptr, const char *name,
@@ -646,7 +563,7 @@ int write_dir(uint32_t inode_no, char *dir_name, mode_t mode) {
     assert(inode_no >= 1 && inode_no <= inode_count);
     
 	struct fs_inode *inode = inode_tbl + inode_no;
-	uint32_t dir_inode = -ENOSPC, data_blk;
+	uint32_t dir_inode = 0, data_blk;
 
 	/* Write to free directory entry in direct pointers */
 	for (int i = 0; i < N_DIRECT; i++) {
@@ -710,17 +627,16 @@ int write_dir(uint32_t inode_no, char *dir_name, mode_t mode) {
 }
 
 int lab3_mkdir(const char *path, mode_t mode) {
-    
-    if(strlen(path) > 27) {
-    	return -ENAMETOOLONG;
-    }
-    
     uint32_t *inode_no = malloc(sizeof(uint32_t));
     *inode_no = 1; // initially, starts from root dir
     /* Read tokens from path through parser */
     char *tokens[MAX_TOKENS], linebuf[1024];
     int n_tokens =
         split_path(path, MAX_TOKENS, tokens, linebuf, sizeof(linebuf));
+    
+    if (strlen(tokens[n_tokens - 1] > 27) {
+    	return -ENAMETOOLONG;
+    }
 
     int res = _getinodeno(n_tokens, tokens, inode_no);
     if (res == 0) {
